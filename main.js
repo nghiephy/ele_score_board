7953c1a8432d8b635712d5d2a2f88f2e1d6c0b01
Nghiep Nguyen <nguyenlapnghiep44@gmail.com>
2023-12-14 00:12:15 +0700

    add api

--- main.js
+++ main.js
@@ -1,56 +1,18 @@
-courses = [
-    {  name: "Vi - Tích phân C", credits: 3, score: 1},
-    {  name: "Pháp luật đại cương", credits: 2, score: 3.5},
-    {  name: "Mác-Lênin 1", credits: 2, score: 3.0},
-    {  name: "Đại số tuyến tính", credits: 2, score: 3.5},
-    {  name: "Sinh học đại cương", credits: 2, score: 3.5},
-    {  name: "TT. Sinh học đại cương", credits: 1, score: 4},
-    {  name: "Vật lý đại cương ", credits: 3, score: 2},
-    {  name: "TT. Vật lý đại cương", credits: 1, score: 4},
-    {  name: "TT. Hóa học đại cương 2", credits: 1, score: 3.5},
-    {  name: "Mác-Lênin 2", credits: 3, score: 1.5},
-    {  name: "Hóa học đại cương 1", credits: 2, score: 1},
-    {  name: "Cơ sở văn hóa Việt Nam", credits: 2, score: 2.5},
-    {  name: "Tư tưởng Hồ Chí Minh", credits: 2, score: 2.5},
-    {  name: "Hóa lý 1", credits: 3, score: 1},
-    // {  name: "Hóa Hữu cơ 1", credits: 3, score: 1},
-    {  name: "TT. Hóa Hữu cơ 1", credits: 1, score: 3},
-    {  name: "Hóa phân tích 1", credits: 3, score: 2},
-    {  name: "TT. Hóa Vô cơ 1 ", credits: 1, score: 2},
-    {  name: "TT. Hóa phân tích 1", credits: 1, score: 3},
-    // {  name: "Hóa vô cơ 1", credits: 3, score: 1},
-    {  name: "TT. Hóa Vô cơ 2", credits: 1, score: 3.5},
-    {  name: "Hóa lý 2", credits: 3, score: 1},
-    {  name: "TT. Hóa lý", credits: 2, score: 1},
-    {  name: "Hóa phân tích 2", credits: 3, score: 1},
-    {  name: "TT. Hóa hữu cơ 2", credits: 1, score: 3.5},
-    {  name: "TT. Hóa phân tích 2", credits: 1, score: 4},
-    // {  name: "Hóa Hữu cơ 2", credits: 3, score: 2},
-    {  name: "Đường lối cách mạng", credits: 3, score: 2},
-    {  name: "Hóa học đại cương 2 ", credits: 3, score: 2},
-    {  name: "Anh văn chuyên môn - Hóa học", credits: 2, score: 1},
-    // {  name: "Hóa sinh học", credits: 3, score: 1},
-    {  name: "Hóa vô cơ 1", credits: 3, score: 2.5},
-    {  name: "Các phương pháp phân tích hiện đại", credits: 3, score: 2},
-    {  name: "Phương pháp Nghiên cứu khoa học - Hóa học", credits: 1, score: 3.5},
-    {  name: "TT. Hóa sinh học", credits: 1, score: 3.5},
-    {  name: "Hóa môi trường", credits: 3, score: 1},
-    {  name: "Nguyên lý bảo quản và chế biến thực phẩm", credits: 2, score: 3},
-    {  name: "Hóa học thực phẩm", credits: 3, score: 2.5},
-    {  name: "Hóa vô cơ 2", credits: 3, score: 1},
-    {  name: "Phức chất", credits: 2, score: 4},
-    {  name: "TT. Các phương pháp phân tích hiện đại", credits: 1, score: 4},
-    {  name: "TT. Hóa môi trường", credits: 1, score: 3.5},
-    {  name: "Các phương pháp phổ nghiệm hữu cơ", credits: 2, score: 2},
-    {  name: "Phân tích kỹ thuật", credits: 3, score: 1.5},
-    {  name: "Hóa sinh học", credits: 3, score: 3.5},
-    {  name: "Hóa hữu cơ 2", credits: 3, score: 3.5},
-    {  name: "Các phương pháp thống kê hóa học", credits: 2, score: 2},
-    {  name: "Hóa Hữu cơ 1", credits: 3, score: 2.5},
-    {  name: "điểm A hk1", credits: 9, score: 4},
-    {  name: "điểm B hk1", credits: 2, score: 3},
-    {  name: "điểm Dp hk1", credits: 3, score: 1.5},
-]
+// courses = [
+//     {  name: "Pháp luật đại cương", credits: 2, score: 3.5},
+//     {  name: "Triết học Mác - Lênin", credits: 3, score: 3},
+//     {  name: "Kinh tế chính trị Mác - Lênin", credits: 2, score: 1.5},
+//     {  name: "Chủ nghĩa xã hội khoa học", credits: 2, score: 1.5},
+//     {  name: "Lịch sử Đảng Cộng sản Việt Nam", credits: 2, score: 2},
+//     {  name: "Tư tưởng Hồ Chí Minh", credits: 2, score: 2.5},
+//     {  name: "Cơ sở văn hóa Việt Nam", credits: 2, score: 2.5},
+//     {  name: "Kỹ năng thuyết trình", credits: 2, score: 3.5},
+//     {  name: "Lý thuyết dịch", credits: 2, score: 3.5},
+//     {  name: "Đọc - Viết B2.1", credits: 3, score: 2.5},
+//     {  name: "Nghe - Nói B2.1", credits: 3, score: 3},
+//     {  name: "Tiếng Anh thư tín thương mại và văn phòng", credits: 3, score: 3},
+//     {  name: "Ngữ âm thực hành cơ bản", credits: 3, score: 3.5},
+// ]
 
 var scoreBoard = document.querySelector('#score-board')
 var nameInput = document.querySelector('#name');
@@ -58,12 +20,11 @@ var creditsInput = document.querySelector('#credits');
 var scoreInput = document.querySelector('#score');
 var displayScore = document.querySelector('#message_score__text')
 
-loadCourses()
-calculator()
+getAllCourses()
 
-function calScore() {
+function calScore(courses) {
     var totalScore=0
-    var totalCredit=0
+    var totalCredit = 0
     courses.forEach(course => {
         totalScore += course.score*course.credits
         totalCredit += course.credits
@@ -71,17 +32,33 @@ function calScore() {
     return (totalScore/totalCredit*1.0).toFixed(2)
 }
 
-function calculator() {
+function calculator(courses) {
     let name = nameInput.value
     let credits = creditsInput.value
     let score = scoreInput.value
     if (name && credits && score) {
-        courses.push({name: name, credits: Number(credits), score: parseFloat(score)})
+        fetch('https://bc-api-yaio.onrender.com/score/add', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({ name: name, credits: credits, score: score })
+        }).then(response => {
+            if (response.ok) {
+                return response.json();
+            }
+            throw new Error('Request failed!');
+        }, networkError => {
+            console.log(networkError.message);
+        }).then(jsonResponse => {
+            loadCourses(jsonResponse.newScore);
+            displayScore.innerText = calScore(jsonResponse.newScore);
+            return;
+        })
     } else {
         alert("Thông tin chưa đủ zk ơi")
     }
-    loadCourses()
-    displayScore.innerText = calScore()
+    displayScore.innerText = calScore(courses)
 }
 
 function alter() {
@@ -93,27 +70,54 @@ function alter() {
     creditsCourse = creditsElement.value
     newScore = newElement.value
 
-    courses[orderCourse-1].score = parseFloat(newScore)
-    courses[orderCourse-1].credits = Number(creditsCourse)
-    loadCourses()
-    displayScore.innerText = calScore()
-    alert("Sửa điểm rồi á zk!")
+    fetch('https://bc-api-yaio.onrender.com/score/update', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({ index: orderCourse - 1, credits: creditsCourse, score: newScore })
+    }).then(response => {
+        if (response.ok) {
+            return response.json();
+        }
+        throw new Error('Request failed!');
+    }, networkError => {
+        console.log(networkError.message);
+    }).then(jsonResponse => {
+        loadCourses(jsonResponse.newScore);
+        displayScore.innerText = calScore(jsonResponse.newScore);
+        alert("Sửa điểm rồi á zk!")
+    })
+
 }
 
 function remove() {
     let orderRemove = prompt("Nhập STT zk muốn xóa nà:")
-    let removeList = courses.splice(orderRemove-1, 1)
-
-    loadCourses()
-    displayScore.innerText = calScore()
-    console.log(courses)
     
-    alert(`A xóa học phần ${removeList[0].name} cho zk rồi á!`)
+    fetch('https://bc-api-yaio.onrender.com/score/delete', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({ index: orderRemove - 1 })
+    }).then(response => {
+        if (response.ok) {
+            return response.json();
+        }
+        throw new Error('Request failed!');
+    }, networkError => {
+        console.log(networkError.message);
+    }).then(jsonResponse => {
+        loadCourses(jsonResponse.newScore);
+        displayScore.innerText = calScore(jsonResponse.newScore)
+        alert(`A xóa học phần ${removeList[0].name} cho zk rồi á!`)
+    })
+
+    
     
 }
 
-
-function loadCourses() {
+function loadCourses(courses) {
     htmls = `
     <tr>
         <th>STT</th>
@@ -134,4 +138,20 @@ function loadCourses() {
     })
 
     scoreBoard.innerHTML = htmls
-  }
+}
+
+function getAllCourses() {
+    fetch('https://bc-api-yaio.onrender.com/score', {
+        method: 'GET'
+    }).then(response => {
+        if (response.ok) {
+            return response.json();
+        }
+        throw new Error('Request failed!');
+    }, networkError => {
+        console.log(networkError.message);
+    }).then(jsonResponse => {
+        loadCourses(jsonResponse.data);
+        calculator(jsonResponse.data);
+    })
+}
